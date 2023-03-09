a601a9a8e824477e5ed642a69f1724a7f5a9714a
biancamrd <e.bibu.e.b@gmail.com>
2023-03-09 21:53:50 +0200

    solved

--- src/ro/ase/acs/main/Main.java
+++ src/ro/ase/acs/main/Main.java
@@ -1,7 +1,18 @@
 package ro.ase.acs.main;
 
+import ro.ase.acs.classes.Orchestrator;
+import ro.ase.acs.classes.ParameterizedDataInserter;
+import ro.ase.acs.classes.ServiceLocator;
+import ro.ase.acs.interfaces.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("main");
+    public static void main(String[] args){
+    ServiceLocator serviceLocator = new ServiceLocator();
+    Orchestrator orchestrator = new Orchestrator(serviceLocator.get(DatabaseConnection.class),
+            serviceLocator.get(TableBuilder.class), serviceLocator.get(DataReader.class),
+            serviceLocator.get(DataWriter.class), serviceLocator.get(DataStorage.class));
+        orchestrator.runWorkflow();
+        orchestrator.setDataStorage(new ParameterizedDataInserter());
+        orchestrator.runWorkflow();
     }
 }
